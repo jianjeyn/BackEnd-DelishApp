71ec3a8f6f9a2f22e4658d9c2eaf36379ec8a160
“klaraollivviera11” <klaraollivviera@upi.edu>
2025-06-12 17:06:11 +0700

    communities and community revisi

--- database/migrations/2025_06_12_094954_create_communities_table.php
+++ database/migrations/2025_06_12_094954_create_communities_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('communities');
+        Schema::dropIfExists('community_user');
     }
 };
