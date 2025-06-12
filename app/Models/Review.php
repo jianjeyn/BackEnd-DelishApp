779c7d2f87b45e73621d7d8d8978079364b7f17a
Naeya Adeani <naeyaap@upi.edu>
2025-06-12 17:40:47 +0700

    step, review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,28 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'resep_id',
+        'user_id',
+        'deskripsi',
+        'bintang',
+    ];
+
+    // Relasi ke model Resep
+    public function resep()
+    {
+        return $this->belongsTo(Resep::class);
+    }
+
+    // Relasi ke model User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Models/Step.php
+++ app/Models/Step.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Step extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'resep_id',
+        'no',
+        'deskripsi',
+    ];
+
+    // Relasi ke model Resep
+    public function resep()
+    {
+        return $this->belongsTo(Resep::class);
+    }
 }
