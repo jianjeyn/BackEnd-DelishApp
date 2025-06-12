254a70a0769ac46e8d1df2e4b13dbb7749ad8e5f
rinmarch12 <ririnmarchelina1234@gmail.com>
2025-06-12 17:15:41 +0700

    commit

--- database/migrations/2025_06_12_094955_create_notification_user_table.php
+++ database/migrations/2025_06_12_094955_create_notification_user_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('notification_user', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('notification_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
