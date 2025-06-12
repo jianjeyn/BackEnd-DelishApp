4fc9c5b9430ed8092253480eda8517293659537c
rinmarch12 <ririnmarchelina1234@gmail.com>
2025-06-12 18:01:26 +0700

    commit

--- database/migrations/2025_06_12_094955_create_recipes_table.php
+++ database/migrations/2025_06_12_094955_create_recipes_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('recipes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('nama');
             $table->string('foto')->nullable();
             $table->text('detail');
