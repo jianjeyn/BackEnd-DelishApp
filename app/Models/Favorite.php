79dccca6e4802df870c57bece34bc9775d1e6ac7
“klaraollivviera11” <klaraollivviera@upi.edu>
2025-06-12 17:50:39 +0700

    community, community user, favorite, follower model

--- app/Models/Favorite.php
+++ app/Models/Favorite.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Favorite extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 
+        'recipe_id'
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function recipe(): BelongsTo
+    {
+        return $this->belongsTo(Recipe::class);
+    }
 }
