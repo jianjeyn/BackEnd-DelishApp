54933b1f5aa5c6689aec4b21dd96a016df0552ec
alipa97 <153807422+alipa97@users.noreply.github.com>
2025-06-14 21:07:53 +0700

    membuat routes api.php

---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group.
+|
+*/
+
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
+
+// Protected routes
+Route::middleware(['auth:sanctum'])->group(function () {
+
+});
